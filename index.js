846928959c0130577fbce897000193650405c83b
LuisDa <luisdarivero.s@gmail.com>
2020-05-08 06:08:10 -0500

    Se crea servidor usando express

---
+++ index.js
@@ -0,0 +1,13 @@
+//Añade variables de entorno para testing local
+require('dotenv').config()
+
+// 1) Obtenemos app y PORT mediante "destructuring"
+// 2) app y PORT vienen del archivo index de la carpeta server
+// NOTA: No es necesario agregar "index" cuando se solicita mediante 
+const { server, PORT } = require('./server');
+
+//TODO: añadir una base de datos en mongo
+//require('./database');
+
+// Encedemos servidor
+server.listen(PORT, () => console.log(`Listening on ${PORT}`));
---
+++ server/index.js
@@ -0,0 +1,24 @@
+//Se declara el servidor usando express
+const express = require('express');
+const server = express();
+//Se define el puerto a utilizar
+const PORT = process.env.PORT || 3000;
+//Se declara celebrate para validar los campos en los middlewares
+const { errors } = require('celebrate');
+//Se añade al servidor la opción de usar json y urlencoded
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+// Endpoints
+
+//TODO: borrar
+server.get('/', (req, res) => res.send('Hello World!')); 
+
+//TODO: añadir rurtas
+//server.use('/api/v1', require('../router'));
+
+//Manejo de errores en el middleware para cachar los errores mandados por celebrate
+server.use(errors());
+
+// exportar server para poder requerirlo desde otros archivos
+module.exports = { server, PORT };
