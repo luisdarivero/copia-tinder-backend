cbde55da1d4b95a275e0069a9462e5286c410938
LuisDa <luisdarivero.s@gmail.com>
2020-05-10 22:59:09 -0500

    Se crea validador de usuario con celebrate para el signup

---
+++ validators/UsersValidator.js
@@ -0,0 +1,15 @@
+const { celebrate, Joi, Segments } = require('celebrate');
+
+module.exports = {
+    signup: celebrate({
+        [Segments.BODY]: Joi.object().keys({
+            email: Joi.string().email().required(),
+            password: Joi.string(),
+            name: Joi.string().required(),
+            gender: Joi.string().required().valid('Mujer').valid('Hombre'),
+            //minimo 18 años de edad
+            birth_date: Joi.date().required().max(Date.now() - (31557600000 * 18)),
+            show_people_with_gender: Joi.string().required().valid('Mujer').valid('Hombre'),
+        }),
+    }),
+};
