f2c0a5926d96014b63e45c4da9792eadf3cb9ad6
LuisDa <luisdarivero.s@gmail.com>
2020-05-10 22:59:36 -0500

    Se añade validador a la ruta para hacer el sign up

--- router/AuthRoutes.js
+++ router/AuthRoutes.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 //Se carga el controlador de usuarios
 const { UsersController } = require('../controller');
+//Se cargan los validadores de usuario
+const { UsersValidator } = require('../validators')
 
-router.post('/users/signup', UsersController.signup);
+router.post('/users/signup', UsersValidator.signup, UsersController.signup);
 //TODO: router.post('/users/login', UsersController.login);
 
 module.exports = router;
