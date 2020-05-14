fd583d7c2cddc73a5c79847c805196929a5978b7
karrli <krla.ici@gmail.com>
2020-05-13 21:33:03 -0500

    Se agrega endpoint para login de usuario (comparar contraseñas)

--- services/UsersService.js
+++ services/UsersService.js
@@ -1,8 +1,12 @@
 const Users = require('../models/Users');
+const bcrypt = require('bcrypt');
 
 module.exports = {
   create: (body) => {
     const newUser = new Users(body);
     return newUser.save();
+  },
+  comparePasswords: (candidatePassword, password) => {
+    return bcrypt.compareSync(candidatePassword, password);
   }
 }
