579b033c5c5f7329cbdb25f4a5cd8e81ee161f89
karrli <krla.ici@gmail.com>
2020-05-14 16:09:24 -0500

    Se actualiza login para usar findByEmail desde el servicio

--- services/UsersService.js
+++ services/UsersService.js
@@ -6,6 +6,11 @@ module.exports = {
     const newUser = new Users(body);
     return newUser.save();
   },
+  findByEmail: (email) => Users.findOne({ email }),
+  update: (user, body) => {
+    Object.assign(user, body);
+    return user.save();
+  },
   comparePasswords: (candidatePassword, password) => {
     return bcrypt.compareSync(candidatePassword, password);
   }
