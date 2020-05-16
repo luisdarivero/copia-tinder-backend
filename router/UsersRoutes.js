970c1268bf1406acc4f799dbc364dadd45401320
LuisDa <luisdarivero.s@gmail.com>
2020-05-16 12:39:12 -0500

    Se agregan rutas para buscar usuarios cercanos

--- router/UsersRoutes.js
+++ router/UsersRoutes.js
@@ -4,6 +4,5 @@ const router = express.Router();
 
 //TODO: borrar, este es solo un ejemplo
 router.get('/users', (req, res) => res.send('Rutas de usuario')/*TODO: agregar controllers*/);
-router.use(require('./NearUsersInteractionRoutes'));
 
 module.exports = router;
