1ea6ee2d4ec7505d9e0d809db0b893c5fba7b36f
LuisDa <luisdarivero.s@gmail.com>
2020-05-19 14:13:59 -0500

    se actualiza el index de servicios

--- services/index.js
+++ services/index.js
@@ -1,7 +1,11 @@
 const UsersService = require('./UsersService');
 const UserPublicInformationService = require('./UserPublicInformationService');
+const ChatsService = require('./ChatsService');
+const MatchesService = require('./MatchesService');
 
 module.exports = {
   UsersService,
-  UserPublicInformationService
+  UserPublicInformationService,
+  ChatsService,
+  MatchesService
 } 
