e7ea8cbbbc4955411b1ef493a265a0ceb5f2768a
karrli <krla.ici@gmail.com>
2020-05-20 19:06:57 -0500

    Agregar manejo de bases de datos segun entorno e instalar cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 //Se declara el servidor usando express
 const express = require('express');
 const server = express();
+const cors= require('cors');
 //Se define el puerto a utilizar
 const PORT = process.env.PORT || 3000;
 //Se declara celebrate para validar los campos en los middlewares
@@ -8,8 +9,10 @@ const { errors } = require('celebrate');
 //Se añade al servidor la opción de usar json y urlencoded
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
+server.use(cors())
 
 // Endpoints: se añaden rutas al servidor
+server.get('/', (req, res) => res.send('Hello World!'));
 server.use('/api/v1', require('../router'));
 
 //Manejo de errores en el middleware para cachar los errores mandados por celebrate
