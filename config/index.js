e781c8ad99da1b7c594661511b460a491fbef74f
karrli <krla.ici@gmail.com>
2020-05-20 22:19:40 -0500

    Se actualiza variable de entorno a production y los node modules

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 const { NODE_ENV, MONGO_USER, MONGO_PASSWORD } = process.env;
 
 const config = {
-  test: {
-    MONGO_URI: `mongodb://${MONGO_USER}:${MONGO_PASSWORD}cinta-negra-shard-00-00-rez02.mongodb.net:27017,cinta-negra-shard-00-01-rez02.mongodb.net:27017,cinta-negra-shard-00-02-rez02.mongodb.net:27017/test?ssl=true&replicaSet=cinta-negra-shard-0&authSource=admin&retryWrites=true&w=majority`
+  production: {
+    MONGO_URI: `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@cinta-negra-shard-00-00-rez02.mongodb.net:27017,cinta-negra-shard-00-01-rez02.mongodb.net:27017,cinta-negra-shard-00-02-rez02.mongodb.net:27017/test?ssl=true&replicaSet=cinta-negra-shard-0&authSource=admin&retryWrites=true&w=majority`
   }
 };
 
